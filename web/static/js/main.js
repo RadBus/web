2add66d49d789645d409e53192a9d5b7810612cb
Peter Stromquist <peter@twistedstream.com>
2014-04-25 15:15:34 -0500

    Move auth logic from /auth/index.html to / and enhance a bit with jQuery

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,1 +1,37 @@
+function onClientLoad() {
+  gapi.client.setApiKey(googleClientSecret);
 
+  setTimeout(function() {
+    gapi.auth.authorize({
+      client_id: googleClientId,
+      scope: googleAuthScopes,
+      immediate: true
+    }, onAuthResult);
+  }, 1);
+}
+
+function onAuthResult(authResult) {
+  var authorizeButton = $('#authorize-button');
+
+  if (authResult && !authResult.error) {
+    authorizeButton.hide();
+
+    var message =
+      "token_type: " + authResult.token_type + "\n" +
+      "access_token: " + authResult.access_token;
+
+    window.alert(message);
+
+  } else {
+    authorizeButton.show();
+    authorizeButton.click(function () {
+      gapi.auth.authorize({
+        client_id: googleClientId,
+        scope: googleAuthScopes,
+        immediate: false
+      }, onAuthResult);
+
+      return false;
+    });
+  }
+}
