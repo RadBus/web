2e51bc271f7da8ec293823a711ec6c4426c43a0c
Peter Stromquist <peter@twistedstream.com>
2014-12-12 13:25:02 -0600

    Fix onAjaxError function to better display errors

--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -51,11 +51,18 @@
   };
 
   function onAjaxError (jqXHR, textStatus, errorThrown) {
-    if (jqXHR.status == 401) {
+    if (jqXHR.status == 401 &&
+        jqXHR.responseJSON &&
+        jqXHR.responseJSON.message &&
+        jqXHR.responseJSON.message.match(/authorization token/i)) {
       // token expired - authorize again
       authorize();
     } else {
-      alert("ERROR: " + jqXHR.status + ": " + textStatus + ": " + errorThrown);
+      var message = jqXHR.status > 0 ?
+        (jqXHR.status + ": " + jqXHR.statusText + ": " + jqXHR.responseText) :
+        "Check the JavaScript Console for details.";
+
+      alert("ERROR: " + message);
     }
   }
 
